1fe6fdb4019ad083947e6fbe5ff4ac6d5b653ecd
Rafael Sousa <rafael.oliveira.osv@gmail.com>
2020-01-18 15:34:44 -0300

    Ajustes

--- src/main/java/br/com/loja/produto/controler/ProdutoController.java
+++ src/main/java/br/com/loja/produto/controler/ProdutoController.java
@@ -2,6 +2,7 @@ package br.com.loja.produto.controler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class ProdutoController {
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
+	@PostMapping
 	public ResponseEntity<Produto> adicionarProduto(@RequestBody Produto produto){
 		produtoRepository.save(produto);
 		return ResponseEntity.ok().body(produto);
